3fd78079e6d43154a0b06935eeee9ee17af51522
lelouisdevn <thaingo1202@outlook.com>
2025-10-05 10:54:58 +0700

    Print the path to destination node

--- algorithms/v2025/dijkstra.c
+++ algorithms/v2025/dijkstra.c
@@ -14,10 +14,13 @@ int Graph[V][V] =
 
 int visited[V];
 int distance[V];
+int prevNode[V];
 
-/* First, find the node that has the lowest score (distance)*/
-/* find its adjacent nodes and updates path if the new distance is smaller */
-/* mark it as visited, so we won't check it again */
+/** Dijkstra takes a Graph and the starting node:
+ *  First, find the node that has the lowest score (distance)
+ *  find its adjacent nodes and updates path if the new distance is smaller
+ *  mark it as visited, so we won't check it again; Also store the previous node of the current node 
+*/
 void Dijkstra(int Graph[][V], int node)
 {
     
@@ -25,6 +28,7 @@ void Dijkstra(int Graph[][V], int node)
     {
         visited[i] = 0;
         distance[i] = 99999999;
+        prevNode[i] = -1;
     }
     distance[node] = 0;
     for (int i = 0; i < V; i++)
@@ -35,30 +39,56 @@ void Dijkstra(int Graph[][V], int node)
         {
             if (visited[j] == 0 && distance[j] < min)
             {
-                //printf("min = %d\n", distance[j]);
                 min = distance[j];
                 v = j;
             }
         }
         visited[v] = 1;
-        printf ("vis %d\n", v);
+        
         for (int z = 0; z < V; z++)
         {
             if (visited[z] == 0 && Graph[v][z] != 0 && distance[v] + Graph[v][z] < distance[z])
             {
                 distance[z] = distance[v] + Graph[v][z];
-                //printf ("dist %d", distance[z]);
+                prevNode[z] = v;
             }
         }
-        
     }
+}
+/** pathFromSrcToDest takes a Graph, destination node, and update it to the int *path and returns the current index:
+ *  prevNode contains previous node for the current node, let the current be dest 
+ *  find from the dest back to the source, and store them to the array int* path
+ *  and when printing out, print them backward to retain the correct order from src to dest
+ */
+int pathFromSrcToDest(int Graph[][V], int dest, int *path)
+{
+    int idx = 0;
+    int curNode = dest;
+    while (curNode != -1)
+    {
+        /* Well, 'path[idx++] = curNode' is the same but it should be 'path[idx] = curNode; idx++' for better clarity */
+        path[idx] = curNode; idx++;
+        curNode = prevNode[curNode];
+    }
+    return idx;
 }
 
 int main()
 {
-    Dijkstra(Graph, 0);
-    for (size_t i = 0; i < 5; i++)
+    int dest = 2;
+    int start = 0;
+    Dijkstra(Graph, start);
+
+    for (int i = 1; i < V; i++)
     {
-        //printf("%d ", distance[i]);
+        int path[V] = {0};
+        int k = pathFromSrcToDest(Graph, i, path);
+        printf ("Path to %d: ", i);
+
+        for (int z = k-1; z >= 0; z--)
+        {
+           printf ("%d ", path[z]);
+        }
+        printf ("\n");
     }
 }
